bcb97b02329029928e2433910767ffb1455d3732
jeevanantham.a <jeevaengg21@gmail.com>
2022-09-08 08:35:46 +0530

    day-10 updates

--- html_basics/scripts/function_sample.js
+++ html_basics/scripts/function_sample.js
@@ -1,14 +1,3 @@
-// private <T> methodName(arg1,arg2){
-
-// }
-
-// private void methodName(arg1,arg2){
-
-// }
-
-
-
-
 function childFunction() {
     console.log("I am a child function")
 }
@@ -28,3 +17,96 @@ function sum(a, b) {
     let c = a + b
     return c;
 }
+
+/** Array as stack */
+/** Array default behaviour - last in first out */
+
+let school = []
+
+school.push("s1")
+school.push("s2")
+
+school.pop()
+
+/** Array as queue */
+
+school = []
+
+school.unshift("x1")
+school.unshift("x2")
+school.unshift("x3")
+school.unshift("x4")
+
+let popped = school.pop()
+console.log(popped)
+console.log(school)
+
+
+/** Conditional Statement */
+let age = 15
+if (age >= 18) {
+    console.log("Major")
+} else if (age < 18 && age > 12) {
+    console.log("teenager")
+} else {
+    console.log("child")
+}
+
+/** Equal comparison operator */
+let x = '1'
+/** === vs == */
+
+/** with == */
+if (1 == x) {
+    console.log("Correct")
+} else {
+    console.log("Wrong")
+}
+
+/** with === */
+if (1 === x) {
+    console.log("Correct")
+} else {
+    console.log("Wrong")
+}
+
+/** != vs !== */
+
+/** with != */
+if (1 != x) {
+    console.log("Correct")
+} else {
+    console.log("Wrong")
+}
+
+/** with !== */
+if (1 !== x) {
+    console.log("Correct")
+} else {
+    console.log("Wrong")
+}
+
+/** global scoping */
+
+let i = 0;
+
+for (i; i < 10; i++) {
+    console.log("for looping count ", i)
+}
+
+
+while (i < 5) {
+    console.log("while looping count ", i)
+    i++
+}
+
+/** shadow scoping - local variable hide the global variable when both having same variable name */
+function printer() {
+    let i = 0
+    console.log("printing i ", i)
+    for (i; i < 15; i++) {
+        console.log("Printing page ", i)
+    }
+
+}
+printer()
